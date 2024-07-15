c8ccccc349bc21ba914aeab78f2ffaa7a706815f
Sergio <sguillem04@gmail.com>
2024-07-15 11:32:09 +0200

    fix users_mysql

--- routes/dbconection/dbconection.js
+++ routes/dbconection/dbconection.js
@@ -6,11 +6,11 @@ config();
 const mysqlConnection = {
     getConection: () => {
         let configuration = {
-            host: process.env.DB_HOST,
-            port: process.env.DB_PORT,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME,
+            host: process.env.MYSQL_HOST,
+            port: process.env.MYSQL_PORT,
+            user: process.env.MYSQL_USER,
+            password: process.env.MYSQL_PASSWORD,
+            database: process.env.MYSQL_NAME,
             timezone : "+00:00"
         }
         return configuration
