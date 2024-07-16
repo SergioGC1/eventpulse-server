a843a823afc9522901192a281b4e098856f5bf17
Sergio <sguillem04@gmail.com>
2024-07-16 12:14:57 +0200

    upda: events (get,put,delete)

--- routes/events/events_controlador.js
+++ routes/events/events_controlador.js
@@ -19,4 +19,65 @@ router.post("/", async (req, res) => {
     }
 });
 
+router.get("/", async (req, res) => {
+    try {
+        let events = await eventsMysql.getEvents();
+        res.status(200).json(events);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const result = validationResult(req);
+        if (!result.isEmpty()) {
+            res.status(400).send({ errors: result.array() });
+            return;
+        }
+        let eventos = await eventsMysql.getEventById(req.params.id);
+        if (eventos.length === 0) {
+            res.status(404).send(`El evento con id ${req.params.id} no existe en la base de datos`);
+            return;
+        }
+        res.json(eventos[0]); 
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.delete('/:id', async (req, res, next) => {
+    try {
+        const result = validationResult(req);
+        if (!result.isEmpty()) {
+            res.status(400).send({ errors: result.array() });
+            return;
+        }
+        let eventos = await eventsMysql.deleteEventById(req.params.id);
+        if (eventos.length === 0) {
+            res.status(404).send(`El evento con id ${req.params.id} no existe en la base de datos`);
+            return;
+        }
+        res.json(eventos[0]); 
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.put('/:id', async (req, res, next) => {
+    try {
+        const result = validationResult(req);
+        if (!result.isEmpty()) {
+            res.status(400).send({ errors: result.array() });
+            return;
+        }
+        let evento = req.body;
+        evento = await eventsMysql.putEventById(req.params.id, evento);
+        res.json(evento);
+    } catch (error) {
+        next(error);
+    }
+});
+
+
 export default router;
